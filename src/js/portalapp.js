0090a3fc65058fe2212f70d889e460604358dd64
Christos Tranoris <tranoris@gmail.com>
2020-04-26 22:44:22 +0300

    Moving to 1.0.2-SNAPSHOT

--- src/js/portalapp.js
+++ src/js/portalapp.js
@@ -8,7 +8,7 @@ var app = angular.module('portalapp', [   'ngCookies', 'ngResource', 'ngRoute',
                                          'angular-loading-bar', 'ngAnimate' ]);
 
 
-var portalversion = '20191002';
+var portalversion = '1.0.2-SNAPSHOT';
 
 app.config(function($routeProvider, $locationProvider, $anchorScrollProvider, cfpLoadingBarProvider) {
 	
